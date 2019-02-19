65a4d2fb1cb5f661a9c8fe8ff4917db0062a866b
haziqAhmed <haziq.ahmed92@gmail.com>
2019-02-19 17:54:36 +0500

    Dictionaries Correction

--- Expense/Utilities and Bills.py
+++ Expense/Utilities and Bills.py
@@ -1,865 +1,341 @@
-Utilities_bills__Tex={Utilities_Mobile-bills__Tex:("Bills",
-"Mobile Bill",
-"Internet Bill",
-"Utility Bills",
-"Mobile bill",
-"Mess Bill",
-"Phone Bill",
+Utilities_bills__Tex=["bills and Utilities",
+"atm tax",
 
-"Mobile Bills",
-
-
-"Phone bill",
-"Bill",
-"Utilities Bills",
-
-"mobile bill",
-"Dukan ja bill khato",
-"Electricity Bill",
-"Electricity bill",
-"Mobile & Net Bills",
-
-"Electric Bill ",
-"Bills/Utilities",
-"bills",
-"Utility bills",
-"Gas bill",
-"Mobile bills",
-"Gas Bill",
-"Internet Bills",
-"bills ",
-"Utility bill",
-"Canteen bill",
-"PTCL Bill",
-
-"INDOOR BILL",
-" Bills",
-"Wood Bill",
-"Bill payment",
-"Internet bill",
-"Bills / Mobile",
-
-"Communication Bills",
-"Electric bill",
-"Electricity Bill ",
-"Bills ",
-"Received bills ",
-"Sim Card Bill",
-"Sohaib Mobile Bill",
-"11. Bills",
-"Bills Recovery",
-"Bill Gas",
-"Company Bills",
-"Electric Bill",
-
-"Net Bill",
-"Phone Bills",
-
-"Cable bill",
-
-"GAS BILL",
-
-"Phone Billing",
-
-"Water Bill",
-"Water bill",
-
-
-"Hina Mobile Bill",
-
-"Mobil Bill",
-"Net bill",
-"BILLS",
-"Bill Paid",
-"Billu daily",
-"Laundry Bill",
-"Old House Bills",
-"Pending Bills",
-"Utility-Bills",
-"Bill Payment",
-"Bill+Fee",
-"Mobile postpaid Bill",
-"Telephone bill",
-"Utility Bill",
-"phone bill",
-"Bill Electricity ",
-"Billing",
-"Bills and Payments",
-
-"Ptcl bill",
-"Syllabus Bills - Stars ",
-"Utilities bill",
-
-
-"Elecric bills",
-"Elecricity bill",
-"Gas Bills",
-"House Bills",
-"Mobile bill ",
-"Monthly Bills",
-"Notes Bills - "
-
-"SSGC Bill",
-
-"Ufone Bills",
-"Utilities and bill",
-"Utilities/Electricity Bill",
-"Utility Bills ",
-"internet bill",
-"BILL",
-"Bill Payments",
-"Bill electric,gas,dish, security",
-"Bills (Mobile Phone)",
-"Bills + Monthly",
-"Bills Payment",
-
-"Fixed but variable bills",
-"KW&SB Bill",
-"Mobile Bill ",
-"Rent & Bill",
-
-
-"Sim bill",
-"Wifi bill",
-
-"Cr. Card bill",
-"Electricity Bills",
-"Fixed Bills",
-"Fone Bills",
-
-"Moblink Bill",
-"Pathao Bill",
-"Salaries & Bills",
-"Saman Bill",
-"Society Bill",
-"Tea Bar Bill",
-"Bill Water",
-"Bill Water ðŸ’¦ Filter",
-"Cafe ximynio Bills",
-"Card Bill",
-
-"Curren Bill(M+B)",
-"Elect bill",
-"Electric @ PTCL bill ",
-"Electric gas bills",
-"Gass bill",
-"Hotel bill",
-"Load Bill",
-"Newspaper bill",
-"PTCl Bill",
-"Paint Bill",
-"Phone bills",
-"Warid bill",
-"gas bill",
-"ptcl bill",
-"water bill",
-"Alfalah CC Bill",
-"BILL PAYMENT",
-"Bijli bill",
-"Bill ",
-"Bill Cable ",
-"Bills & Rent",
-"Bills received from Tenants",
-"Bills(electric,gas,water, security,d",
-"Bills.",
-"CC bill",
-"Cable & Internet Bill",
-"Elec/Gas Bills",
-"Electric Bills",
-"Expense Bill ",
-"Fixes Bills(milk n ptcl)",
-"Gas/Elec/Phone Bills",
-"HOME BILLS",
-"Internet (Zong Wifi) Bill",
-"MOBILE BILL TO MR AWAIS",
-"Makanic bill",
-"Monthly Bill",
-"Office Mess Bill",
-
-"Postpaid bill",
-"Ptcl Bill",
-"Sui Gas Bill",
-"Taxi bills",
-"Teacher salary, peon,rent, bill",
-"Utilities & Bills",
-"Utilities/Bills",
-"Water Bills ",
-
-"hotal bill",
-"Askari bank bill",
-
-"BILLs",
-"BN Various Bills",
-"Bill (Cable)",
-"Bill (Electricity)",
-"Bill (Internet)",
-"Bill Paymet",
-"Bill mobilink moon",
-"Billings & Dues",
-"Bills4",
-
-"Cable Bill",
-"Canteen Bill",
-
-"DSL Bills",
-"Eating in (bill)",
-"Edu Fee Bills",
-"Elec Bill",
-"Electrcity bill",
-"Electricity Bill Payments",
-"Fesco bill",
-"Gas Bill Payments",
-"Gass Bill ",
-"Gepco bill",
-"Home Internet & Tv Cable Bill",
-"Hotel Bill",
-"Husnain k bill",
-"Icici bill",
-"Internet & Cable Bill",
-"Mess bill ",
-"PTCL BILL",
-"PTCL bill",
-"Phone bills ",
-"Sui Gas Bill ",
-"Tea bar bill",
-"Utilities bills",
-"Utility bills ",
-"mobile bill ",
-"Bijli Bill",
-
-"Dish bill",
-"ELECTRIC BILL",
-"Electricity bill ",
-"Electricity bills",
-"GAS Bill",
-"Gas bills",
-"Gass Bill",
-"Internet bills",
-"Mobile Bills ",
-"Monthly bills",
-"Newspaper Bill",
-"Sui gas bill",
-"Utilities BILL",
-"Warid Bill",
-"Water bill ",
+"bijli bill",
+"bil",
 "bill",
+"bill electric",
+"bill electric gas dish security",
+"bill electric gas water security",
+"bill electricity",
+"bill gas",
+"bill monthly",
 "bill payment",
+"bill paymet",
+"bill receive",
+"bill",
+"bill",
+"bill utility",
+"bill",
+"billing",
+"billing due",
+"bill",
+"bought",
+"bill",
+"clinic utility",
+"company bill",
+
+"dsl bill",
 "elec bill",
-"electricity bill",
-"telephone bill",
-" Gas Bill",
-"Bill Electricity",
-"Bill electricity",
-"Bill paid",
-"Bill water",
-"CC Bill",
-"Cable BILL",
-
-"ELECTRICITY BILL",
-"Elec bill",
-"Electric bill ",
-"Fixed bills",
-"GASS BILL",
-"Gas bill ",
-"Home Bills",
-"Home Bills ",
-"Hotal bill",
-"Hotel BILL",
-"Internet Bill ",
-"Internet bill ",
-"MOBILE BILL",
-"Mobil bill",
-"Mobile BILL",
-"Mobile Postpaid Bill",
-"PHONE BILL",
-"PTCL BIll",
-"PTCL bill ",
-"PTCl bill",
-"Phone BILL ",
-"Ptcl BILL",
-"Ptcl Bill ",
-"Received bills",
-"SSGC BILL",
-"Society bill",
-"TELEPHONE BILL",
-"Telephone Bill",
-"UTILITY Bills",
-"Ufone Bills ",
-"Utilities Bill",
-"WATER BILL",
-"Water BILL",
+"elec gas bill",
+"elecric bill",
+"elecricity bill",
+"elect bill",
+"electrcity bill",
+"electric",
 "electric bill",
-"gas Bill ",
-"gas bills",
-"mess bill",
-"net bill",
-"newspaper bill",
-"water bills",
-"Mobile",
-"Mobile Load",
-"Mobile ",
-"Mobile Balance",
-"Mobile Recharge",
-"Mobile recharge",
-"Mobile load",
-"Mobile balance",
-"Mobile card",
-"Mobile Recharge ",
-"Mobile Credit",
-"Mobile Expense",
-"Mobile credit",
-"Mobile Expenses",
-"Mobile Top Up",
-"Mobile Phone",
-"Mobile Cards",
-"Mobile topup",
-"mobile",
-"Mobile Card",
-"Mobile cards",
-"MOBILE BALANCE",
-"MOBILE",
-"Mobile expense",
-"Mobile balance ",
-"Mobile top up",
-"Mobile expenses",
-"Mobile intranet",
-"Mobile Topup",
-"Mobile Exp",
-"Mobile Top-up",
-"mobile load",
-"Mobile Balance ",
-"Mobile load ",
-"Mobile Blance",
-"Mobile Credit ",
-"Mobile Load ",
-"Driver Mobile Load",
-"Mobile Cards Loadd",
-"Mobile loads",
-"Mobile phone",
-"Mobile charge",
-"Automobile",
-"Automobile ",
-"Mobile & internet",
-"Mobile CR",
-"Mobile Top-Up",
-"Mobile Expense ",
-"Mobile/Internet",
-"Mobile Exp.",
-"Mobile Expenses ",
-"Mobile exp.",
-"Mobile & Net",
-"Mobile Installment",
-"Mobile prepaid",
-"Mobile & Internet ",
-"Mobile Top up",
-"Mobile exp",
-"Mobile/Evo top up",
-"Mobiles",
-"Mobile Loud",
-"Mobile and internet",
-"Mobile Accessories",
-"Recharge bus and mobile",
-"Uzma mobile recharge",
-"Mobile & Internet",
-"Mobile card ",
-"Mobile lood",
-"Mobile recharge ",
-"Mobile ðŸ“± ",
-"Personal for Mobile",
-"Sana Mobile",
-"mobile top up",
-"Mobile + Evo",
-"Mobile Top Up (others) ",
-"Mobile installment",
-"Mobile+net use",
-"Ok mobile",
-"balance mobile",
-"Mobile Easy Load",
-"Mobile Phone Expenses ",
-"mobile card",
-"Chupamobile",
-"Mobile Balanace",
-"Mobile Expanse ",
-"Mobile Load Expense",
-"Mobile Computer & oth",
-"Mobile Computer Internet",
-"Mobile repair",
-"Utilities / Mobile",
-"Zubair Mobile Load",
-"Mobile Accessories ",
-"Mobile Allowance",
-"Mobile Cost",
-"Mobile Pbone",
-"Mobile Phone ",
-"Mobile Recharges",
-"Mobile and Internet",
-"Mobile phones & Acces",
-"Mobile/Jazz Cash",
-"Uzair Mobile Topup",
-"Haseeb Mobile",
-"Mobile Charge",
-"Mobile Easy Load & Other",
-"Mobile Internet",
-"Mobile blance",
-"Mobile etc",
-"Mobile pay",
-"Net cards mobile balance",
-"Rep & Maint Mobiles",
-"Umema Mobile Load",
-"mobile recharge",
-"AUTOMOBILE EXPENSES",
-"Ami Mobile cards",
-"Anam mobile Topup",
-"Mobile Exp. ",
-"Mobile Lord",
-"Mobile Payments",
-"Mobile bil",
-"Mobile sale purchasr",
-"Mobile shop",
-"mobile ",
-"Automobile tuning",
-"Credit (Mobile) ",
-"Mobile Allowance ",
-"Mobile Charges",
-"Mobile Phone Balance",
-"Mobile Recharge u",
-"Mobile accessories ",
-"Mobile and accessories",
-"Mobile pkg",
-"mobile phone payments",
-" Mobile Recharge",
-"Easy load mobile card",
-"MI A1 Mobile",
-"MObile Balance",
-"Mobile (Nishu)",
-"Mobile Charge ",
-"Mobile Dues - Mi",
-"Mobile Installment ",
-"Mobile LOAD",
-"Mobile Lode",
-"Mobile balance & Internet",
-"Mobile/ Communication",
-"mobile phone",
-" Mobile Recharge ",
-"Internet & Mobile",
-"Jazz cash mobile account",
-"Mobile & Internet Expenses",
-"Mobile Instalments",
-"Mobile Profit",
-"Mobile Recharge & Data Package",
-"Mobile Topup/Recharge",
-"Mobile balnace",
-"Mobile calling",
-"Mobile charge ",
-"Mobile cover",
-"Mobile re-charge",
-"New mobile",
-"Telephone/mobile",
-"Bmobile",
-"Installment Mobile ",
-"Internet Mobile",
-"MOBILE-INTERNET",
-"Mobile Allownce ",
-"Mobile Charges ",
-"Mobile Dues - Samsung",
-"Mobile Recharg",
-"Mobile accessories",
-"Mobile ceiling",
-"Mobile charges",
-"Mobile ledger ",
-"Mobile n net",
-"Mobile package ",
-"Mobile repairing",
-"Mobile sale",
-"Mobile top Up",
-"Mobiles ",
-"Omni Mobile Account",
-"Recharge mobile",
-"Rehan mobile ",
-"mobile card ",
-"mobile new",
-"ðŸ“± Mobile",
-"Ad mobile balance",
-"Anusha mobile balance",
-"BKASH Mobile",
-"Balance mobile",
-"Chanda Mobile",
-"Easy Load Mobile",
-"Internet & Mobile ",
-"Jio Mobile Recharge ",
-"MOBILE ",
-"MOBILE LOAD",
-"Mobile Repair",
-"Mobile TopUp",
-"Mobile expenses ",
-"Mobile phone ",
-"Mobile/internet",
-"mobile & internet",
-"mobile credit",
-"mobile instalments",
-"new mobile",
-"MOBILE BALANCE ",
-"MOBILE CARDS",
-"MOBILE TOPUP",
-"Mobile Package",
-"Mobile REcharge",
-"Mobile Recharges ",
-"Mobile Repair ",
-"Mobile Top up ",
-"Mobile credit ",
-"Mobile installment ",
-"Mobile internet",
-"Mobile ledger",
-"Mobile package",
-"Mobile recharg",
-"Mobile top up ",
-
-"New Mobile",
-"mobile accessories",
-"mobile balance ",
-"AutoMobile",
-"Installment Mobile",
-"Installment mobile",
-"Internet & mobile",
-"MOBILE EXP",
-"MOBILE EXPENSE",
-"MOBILE RECHARGE",
-"Mobile Allownce",
-"Mobile Blance ",
-"Mobile Card ",
-"Mobile Cards ",
-"Mobile Cover",
-"Mobile Cover ",
-"Mobile Internet ",
-"Mobile Loads",
-"Mobile Repairing",
-"Mobile Shop",
-"Mobile allownce",
-"Mobile cards ",
-"Mobile cost",
-"Mobile expense ",
-"Mobile lode",
-"Mobile loud",
-"Mobile new",
-"Mobile recharges",
-"Mobile repair ",
-"Mobile sale ",
-"Mobile top-up",
-"Recharge Mobile",
-"mobile Card",
-"mobile balance",
-"mobile cards",
-"mobile cover",
-"mobile exp",
-"mobile repair ",
-"mobile repairing",
-"mobiles",
-
-
-"Easy load",
-"Easyload",
-"Easy Load",
-"Easy Paisa",
-"Easypaisa",
-"Easy load ",
-"EasyLoad",
-"easy load",
-"Easy paisa",
-"Easy load (I)",
-"Easypaisa ",
-"Easy Load ",
-"Easy load (She)",
-"Easyloads",
-"easy paisa",
-"Mobile easyload",
-"stand easy",
-"Easyload ",
-"EASY PAISA",
-"Stand Easy ",
-"Easy load awais",
-"EasyCard",
-"EASY LOAD",
-"Jazz Easy Load",
-"easyload",
-"Mobile Easyload",
-"Stand easy",
-"Easy LOAD",
-"Mobile EasyLoad",
-"EasyPaisa",
-"Easycard",
-"easypaisa",
-"Light bill",
-"Zong Bills",
-"Maid bill",
-"Bill payement",
-"Bua bill",
-"Cabel bill",
-"Cell phone bill",
-"KE Bill",
-"Mobilink Bill",
-"Mobilink bill",
-"Personal mobile bill",
-"Servent bill",
-"TV Cable Bill",
-"Kotak bill",
-"Lanka Bangla Bill Adjustment",
-"Zong bills",
-"Bua Bill",
-"Utilities",
-"Utilities ",
-"Utility",
-"Utility ",
-"Clinic utility",
-"Utility KESC",
-"Utility KWSB",
-"Utility SSGC",
-"Utility Internet",
-"Utility TV cable",
-"Utilities u super",
-"Room rent and utilities",
-"Utility U super",
-"Utilities home",
-"Utilities and water",
-"Utilities/Miscellaneous ",
-"NextHome Utilities",
-"Communication Utilities",
-"Rent & Utilities",
-"Utilities Pindi",
-"Utilities for MinDoktor",
-"Rent and Utilities ",
-"Mistry & Utilities",
-"Mobile Utility ",
-"utility",
-"Recharge",
-"Recharge card",
-"Recharge Card",
-"Recharge ",
-"Phone Recharge",
-"recharge",
-"Phone recharge",
-"Recharges",
-"Balance recharge",
-"Recharge card ",
-"Metro Recharge",
-"Recharge Jio",
-"Jio recharge",
-"Phone recharge ",
-"UFone Recharge",
-"RECHARGE",
-"phone recharge",
-"Metro recharge",
-"Phone Recharge ",
-"Fone Recharge",
-" Recharge ",
-
-"Balance",
-"Load",
-"Flexiload",
-"Mob Load",
-"Reload",
-"Flexi Load",
-"Mbl load and package ",
-"EVO load",
-"Telenor Load",
-"Warid Load",
-"FlexiLoad",
-"E/Load",
-"Jazz load ",
-"Load ",
-"Loading",
-"Uload",
-"Zong Load",
-"Zong load",
-"Huma Mobile Load",
-"Jazz Load",
-"Phone load",
-"Waseem Mobile Load",
-"FLEXI LOAD",
-"Flexy load",
-"Zubair zong sim Load",
-"CMLoad Buy",
-"flexiload",
-"Phone",
-"Telephone",
-"Cell phone",
-"Phone ",
-"phone",
-"Phone Expenses",
-"Telephone ",
-"Cellphone",
-"Telephone & Internet",
-"Phone credit",
-"Phone card",
-"Phone Card",
-"Phone Credit ",
-"Cell Phone",
-"Phone Credit",
-"Phone Balance",
-"Phone Exp",
-"Phone accessories",
-"Phone charges",
-"Phone repair",
-"Phones",
-"phone ",
-"Telephone Expense ",
-"Libya Phone",
-"Phone card ",
-"Cellphone balance",
-"Telephone & Data",
-"internet + phone",
-"Net n phone",
-"Phone Bal",
-"Phone & Internet ",
-"Phone airtime/bundles and accessorie",
-"Telephone Recharge",
-"phone lod",
-"Bought phone",
-"Mobile Phone Accessories ",
-"Mobile Phone Recharge",
-"PHONE AND INTERNET",
-"Phone & Internet",
-"Phone Cards/Loads",
-"Phone expenses",
-"Phone qist",
-"Phone+Net Bill",
-"Sunny against iphone 6 ",
-"Telephones",
-"phone charge",
-"Cell Phone card",
-"Cell Phones",
-"Phone credit ",
-"phone balance",
-"Cell phone ",
-"Cell phone card",
-"Mobile Phone Recharge ",
-"Phone Accessories ",
-"Phone Charges",
-"Phone and internet",
-"Phone charge",
-"TELEPHONE",
-"cell phone ",
-"cellphone",
-"phone & internet",
-"telephone expense",
-"SUI Gas",
-
-"Tax",
-"Toll Tax",
-"Toll tax",
-"Taxes",
-"Taxes ",
-"TAX",
-"Tool tax",
-"tax",
-"Tool Tax",
-"Toll Tax ",
-"Toll taxs",
-"Token / Tax",
-"Toll tax ",
-"Tax ",
-"Tax Consultancy",
-"ATM transaction tax",
-"Transaction Tax",
-"ATM Tax",
-"Tex",
-
-
-"Electricity",
-"Electricity ",
-"Electric",
-"Electrician",
-"Electrical",
-"ELECTRICITY",
-"Electric Material",
-"Electric ",
-"Electric work",
-"K Electric",
-"Electrical goods",
+"electric charge",
+"electric gas bill",
+"electric item",
+"electric material",
+"electric matirial",
+"electric saman",
+"electric contract",
+"electrical",
+"electrical electronic",
+"electrical",
+"electrical item",
+"electrician",
 "electricity",
-"Electrical items",
-"Electrician ",
-"Electriction",
-"electric ",
-"Electric and sanitary contract ",
-"Electric material",
-"Electricity and plumbing",
-"Electric Item ",
-"ELECTRIC MATIRIAL ",
-"Electric saman",
-"Electric items",
-"Saif Ullah electric",
-"Electric Charges",
-"Electrical & Electronic",
-"Electricity - Bambalapitiya",
-"Electricity New Home",
-"Mataji electricals",
-"Thakurji electricity",
-"Water and electricity",
-"Water, electricity, tv",
-"Bill electric",
-"Different electric goods",
-"Electric Work",
-" Electricity",
-"Bill Electric",
-"Electric Work ",
-"Electric material ",
-"Electrical ",
-"Communication",
-"Communication ",
-"Telecommunication",
-"Communications",
-"Commute",
-"Communication Expense",
-"Internet ",
-"internet",
-"Zong internet",
-"Internet Charges",
-"Internet fee",
-"Internet charges",
-"Internet Payment",
-"Internet Fee",
-"Home internet",
-"INTERNET FEES",
-"Internet Expense",
-"Internet Fees",
-"Internet package ",
-"Internet & Cable TV",
-"Internet Package ",
-"Internet fees",
-"Internet zong",
-"Internet / Top up ",
-"Internet and cable",
-"Internet service",
-"Internet/Mobile",
-"InterNet Fees",
-"Internet Bii",
-"Internet-Cards ",
-"Internet & Technology",
-"Communication & Internet",
+"electricity bambalapitiya",
+"electricity bill",
+"electricity bill" ,
+"electricity new",
+"electricity plumbing",
+"electriction",
+"gas bill",
+"gas bill payment",
+"gas elec bill",
+"gepco bill",
+"hina bill",
+"hotal bill",
+"hotel bill",
+"k electric",
+"ke bill",
+"kotak bill",
+"kw sb bill",
+"laundry bill",
+"ledger",
+"libya",
+"light bill",
+"bill",
+"makanic bill",
+"mataji electricals",
+"mbl package",
+"mess bill",
+"metro",
+"mistry utility",
+"newspaper bill",
+"utility",
+"note bill ssgc bill",
+"bill",
+"old bill",
+"paint bill",
+"pathao bill",
+"pending bill",
+"receive bill",
+"utility",
+"electric",
+"bill",
+"saman bill",
+"sim bill",
+"society bill",
+"ssgc bill",
+"sui gas",
+"sui gas bill",
 
-"Calling and Internet ",
-"Internet urgent",
-"Online internet",
-"Internet Dish & News",
-"Internet Expense ",
-"Internet and Cell Expense",
-"Internet device",
-"Internet ðŸ“¶ ",
-"Mobile internet pkg",
-"ZONG Internet Bill",
-"BILLS (MOBILE .INTERNET",
-"Bill Internet ",
-"Cable & Internet",
-"Cable & internet ",
-"Cable & internet fee",
+"telephone bill",
+"electricity",
+"utility",
+"utility bill",
+"utility electricity bill",
+"utility kesc",
+"utility kwsb",
+"utility mindoktor",
+"utility miscellaneous",
+"utility pindi",
+"utility ssgc",
+"utility tv",
+"utility u super",
+"utility water",
+"water bill",
+"water electricity",
+"water electricity tv",
+"wifi bill",
+"wood bill"]
 
-"Home Internet",
-"Internet Service",
-"Internet expense",
-"INTERNET ",
-"Internet Package",
-"Internet Service ",
-"Zong Internet",
-"Zong Internet Bill",
-"bill internet")
-                      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
