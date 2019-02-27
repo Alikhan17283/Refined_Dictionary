8b283896a71a31c657be763a95f19ec15f2a9352
Alikhan17283 <47591542+Alikhan17283@users.noreply.github.com>
2019-02-27 16:23:02 +0500

    Update Grocery.py

--- Expense/Grocery.py
+++ Expense/Grocery.py
@@ -1,83 +1,209 @@
-Grocery=[
-
-"bakery",
-"bakery samman",
-"bread",
-"bread rusk bun egg",
-"chicken",
-
-"chicken piece",
-"chicken powder",
-"chicken powder chat masala",
-"chikn bread",
-"coke",
-"cold drink",
-"cold drink refreshment",
-"coldrink chip",
-"daal",
-"daal chawal cheni",
-"dahi egg",
-"daily gost sabzi",
-"doodhwala",
-"egg",
-"egg bread",
-"egg dahi slice",
-"fish",
-"fish meat",
-"fish vegetable",
-"forut sabzi",
-"frout sabzi",
-"fruit",
-"fruit n vegetable",
-"fruit n vegitables",
-"fruit nd chicken",
-"fruit sabzi",
-"fruit sbze",
-"fruit snack",
-"fruit vagetables",
-"fruit veg",
-"fruit veges",
-"fruit veges meat",
-"fruit vegetable",
-"fruit vegetable misc",
-"fruit veggie",
-"fruit veggie dry fruit",
-"fruit vegi",
-"fruit vegis",
-"fruit vegitables",
-"fruit vegitablr",
-"fruite",
-"makan",
-"maryam sabzi",
+Grocery={"Meat":(
 "meat",
-"meat chicken",
+"Chickem",
+"mutton"
+"beef"
+"fish"
+"mince meat"
+"Red Meat",
+"Meat & Poultry"
+"Fish & Meat",
+"Ghosht",
+"Ghost",
+"murgi",
+"bakra"
+),
 
-"milk curd",
-"milk curt",
-"milk dehi",
-"milk dhahi",
-"milkman",
-"milkman monthly",
-"nagori milk",
-"rashan",
-"refreshment",
-"sabzi fruit",
-"sabzi ghost",
-"snack",
-"snack biscuit chanachur",
-"snack ice cream juice",
-"snack juice",
-"snack meal",
-"snack n drink",
-"soda salaf grocery home",
-"tea cold",
-"tea coldrink",
-"tea milk",
-"tea n milk",
-"veg fish fruit",
-"veg fruit",
-"veg fruit meat",
-"veg fruite meet",
-"vegetable masal tomator lasun"
 
-]
+"Fruites & Vegetables":(
+    "fruit"
+    "apple",
+    "banana",
+    "orange",
+    "apricot",
+    "avacado",
+    "grapefruit",
+    "grapes",
+    "guava",
+    "lychee"
+    "lichi",
+    "pineapple",
+    "pomegranate",
+    "watermellon",
+    "rasberries",
+    "fruit",
+    "fruite",
+    "fruites",
+    "froot",
+    "frot",
+    "aloo",
+    "piyaaz",
+    "temator"
+    "gobi",
+    "palak",
+    "dhania",
+    "gajar",
+    "loki",
+    "onions"
+    "potatos",
+    "reddish",
+    "carrot",
+    "cabbage",
+    "cauliflower",
+    "broccoli",
+    "lettuce",
+    "ginger"
+    "pumpkin",
+    "tomatoes"
+    "cucumber",
+    "veg",
+    "vegitable",
+    "vegetable",
+    "vagetabel",
+    "phal",
+    "phaal",
+    "sabzi"
+    "sbzi"
+    "aloo",
+    "piyaz"
+    "sabze"
+    "dates",
+    "dryfruit",
+    
+),
+
+"Dairy products":(
+    "Milk",
+    "doodh",
+    "butter"
+    "makan",
+    "egg"
+    "cream",
+    "yougat",
+    "dahi",
+    "curt",
+    "dehi",
+    "curd",
+    "bread",
+    "double roti",
+    "everyday",
+    "Noorpur Butter",
+    "Nurpur Butter"
+    "custard",
+    "pudding",
+    "ice cream",
+    "cake",
+    "chocolate"
+    "desserts"
+),
+    "pantry":(
+        "rashan",
+        "pantry",
+        "flour",
+        "coffee",
+        "sugar"
+        "cheeni",
+        "aata",
+        "floor",
+        "jelly",
+        "brown rice"
+        "rice"
+        "chawal",
+        "macaroni",
+        "spaghetti",
+        "spageti",
+        "noodel",
+        "noodles",
+        "maggi"
+        "noodel",
+        "daal",
+        "daleya"
+
+
+    ),
+"spices":
+    (   "spices",
+         "oil",
+        "cooking oil",
+        "tail",
+        "masala",
+        "vinegar",
+        " cocoa",
+        "vanila"
+    ),
+"snacks":(
+    "biscuits",
+    "chocolate",
+    "chips",
+    "biscut",
+    "cookies",
+    "bun",
+    "bakery"
+    "nuggets"
+    "kitkat",
+    "waffers",
+    "rusk",
+    "cupcakes",
+    "fries",
+    "nutella",
+    "cereal",
+    "milo",
+    "cold drinks"
+    "pepsi",
+    "7up",
+    "mirinda",
+    "coke",
+    "soft drinks"
+)
+    "Cosmetic":(
+        "soap",
+        "sabun",
+        "facewash",
+        "toothpaste",
+        "toothbrush",
+        "shampoo",
+        "shampo"
+        "bodyspray",
+        "perfume",
+        "prfume",
+        "razor",
+        "blade"
+        "shave"
+        "gillette"
+        "parfume",
+        "conditioner",
+
+    ),
+    "miscellaneous":("soda sarf",
+                     "sarf",
+                     "powder",
+                     "ketchup",
+                     "mayonnaise"
+                     "Drinking water"
+                     "sauces"
+                     "chilly powder"
+                     "fenia"
+                     "pampers",
+                     "tissues",
+                     "napkins",
+
+                     )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
