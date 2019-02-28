324b1342738379bc695c29d58b700a41b7c8294c
Alikhan17283 <47591542+Alikhan17283@users.noreply.github.com>
2019-02-28 10:54:51 +0500

    Update Transport.py

--- Expense/Transport.py
+++ Expense/Transport.py
@@ -1,57 +1,68 @@
 Transport={
 "Cab":("Careem"
-          "carem"
-          "careem"
-          "caream"
-          "crem"
-          "cerem"
-          "careem fair",
-            "uber",
-        "ubar",
-        "ubeer",
-        "ubbar",
-        "ubber",
-       "bykea",
-       "bykia",
-       "ceb",
-       "ola",
-       "taxi",
-       "paxi",
-       "pexi",
-       "taxify"
-       "cab"
+"carem"
+"careem"
+"caream"
+"crem"
+"cerem"
+"careem fair",
+"uber",
+"ubar",
+"ubeer",
+"ubbar",
+"ubber",
+"fare",
+"fair"
+"bykea",
+"bykia",
+"ceb",
+"ola",
+"taxi",
+"paxi",
+"pexi",
+"taxify"
+"conveyance",
+"Convence",
+"Convenience",
+"Conveyance",
+"Convense",
+"Conveyance",
+"Conveyances",
+"cab"
 
-          ),
+),
 "bus":("bus",
-       "metro bus",
-       "bas",
-       "daweoo",
-       "daewo",
-       "bus ferry",
-       "toll",
-       "couch",
-       "shuttle"
-       "daewoo",
-       "van",
-       "coaster",
-       "redbus",
-       "point",
+"metro bus",
+"bas",
+"daweoo",
+"daewo",
+"bus ferry",
+"toll",
+"couch",
+"shuttle"
+"daewoo",
+"van",
+"coaster",
+"redbus",
+"point",
 
-       ),
+),
 
 "Rickshaw":("rickshaw",
-            "auto",
-            "ricksha",
-            "Rickshae",
-            "rakshaw"
-            "rikshwa",
-            "autorickshaw",
-            ),
+"auto",
+"ricksha",
+"Rickshae",
+"rakshaw"
+"rikshwa",
+"autorickshaw",
+),
 
 
 
 "Train":("train",
-         "tran")
+"tran",
+
+)
 
 
 }
