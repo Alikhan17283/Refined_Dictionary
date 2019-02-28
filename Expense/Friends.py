47618bcc89e833f025b684426820116616ffe5c0
aishaslam <23521600+aishaslam@users.noreply.github.com>
2019-02-28 16:36:54 +0500

    Create Friends.py

---
+++ Expense/Friends.py
@@ -0,0 +1,23 @@
+"Friends" = {
+"Friends" : (
+"friend",
+"frnd",
+"fnd",
+"freind",
+"frend",
+"yaar",
+"buddy",
+"pal",
+"buddy",
+"mate",
+"dost",
+"dosti",
+"yaari",
+"yaarian",
+"scene",
+"boys k saath",
+"with friends"
+
+
+)
+}
