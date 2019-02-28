fb4811f52ddfe7f97311ec944702f24381469578
aishaslam <23521600+aishaslam@users.noreply.github.com>
2019-02-28 16:32:57 +0500

    Create Gifts.py

---
+++ Expense/Gifts.py
@@ -0,0 +1,19 @@
+"Gifts" = {
+
+"Gifts":(
+"gift",
+"gifts",
+"tohfa",
+"presents",
+"present",
+"hadiya",
+"gift for kids",
+"surprise",
+"birthday",
+"bouquet",
+"salami",
+"gift card",
+"giftcard"
+
+)
+}
