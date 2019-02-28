d14e1297e06d666808c4e14fa533d14c921df725
aishaslam <23521600+aishaslam@users.noreply.github.com>
2019-02-28 17:26:06 +0500

    Update Shopping.py

--- Expense/Shopping.py
+++ Expense/Shopping.py
@@ -4,29 +4,34 @@ Shopping={
 "cloth assesories",
 "clothe",
 "clothes",
-"clothes shoe",
 "shopping",
 "clothing",
-"cut shoe polish",
+"kurta",
+"baby dress",
+"shalwar kameez",
+"new saari",
+"saaree",
 "dress",
 "dress cloth",
-"dress press",
-"dress shoe",
 "dress shopping",
 "eid shopping",
 "family shopping",
 "marriage shopping",
-"marriage shopping arsal",
 "shadi shopping",
-"shoe",
-"shoe cloth",
-"shoe makeup",
-"shoe polish",
 "shopping",
 "shoping",
 "shooping",
 "wedding clothes"
 )
+  
+"Shoes":(
+"shoe",
+"shoe cloth",
+"shoe makeup",
+"shoe polish",
+"dress shoe",
+"clothes shoes",
+)  
   
 }
 
