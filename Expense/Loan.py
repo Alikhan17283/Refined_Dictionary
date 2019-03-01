2801f2e2c48c61e397c74301622153fc53648b0f
Alikhan17283 <47591542+Alikhan17283@users.noreply.github.com>
2019-03-01 11:34:03 +0500

    Update Loan.py

--- Expense/Loan.py
+++ Expense/Loan.py
@@ -1,24 +1,75 @@
-loan = [
-    "borrow",
+Loan={
+    "Personal Loan":("borrow",
     "borrower",
     "borrowing",
     "borrows",
     "loan",
-    "loan",
-    "ko udhar",
-    "lending borrowing",
-    "loan",
+    "udhar",
     "lone",
     "loen",
-    "leone"
-
-    "saddamloan give",
-    "udhar",
+    "udhar"
+    "loan refund"
+    "bank loan",
     "udhar return",
     "udhar wapis",
+    "loan to others",
     "uudhar",
+     "udari",
+     "uudari",                
+    "repayment of loan",
+    "loan wapis",
+    "loan deyay",
     "karza",
     "kerza",
     "karzaa"
+    "mobile loan"),
 
-]
+    
+    "Home Loan":("Home loan",
+                 "house loan",
+                 "appartment loan",
+                 "flat loan",
+                 "housing loan"
+                 "flate loan",
+                 "zameen loan",
+                 "plot loan",
+                 "ghar loan",
+                 "makaan loan"
+                 "hbfc loan"
+                 ),
+
+    "Vehical Loan":("car loan"
+                    "gari loan",
+                    "bike loan",
+                    "bus loan",
+                    "auto loan",
+                    "moterbike loan",
+                    "motercycle loan",
+                    "vehical loan",
+                    "gaari loan"
+                    ),
+
+    "Business Loan":("Office loan",
+                     "ofc loan",
+                     "shop loan",
+                     "bussiness loan",
+                     "business loan",
+                     "karobar loan",
+                     'carobar loan',
+                     "startup loan"
+                     
+
+
+                     ),
+
+
+
+    "Educational Loan":("school loan",
+                        "college loan",
+                        "study loan",
+                        "educational loan",
+                        "university loan",
+                        "parhaye loan",
+                        "edu loan"
+                        )
+}
