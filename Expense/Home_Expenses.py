041705dff62363bdbfabf57ecbffaa7b6e49f484
aishaslam <23521600+aishaslam@users.noreply.github.com>
2019-03-04 11:44:00 +0500

    Create Home_Expenses.py

---
+++ Expense/Home_Expenses.py
@@ -0,0 +1,60 @@
+Home_Expenses = {
+
+"Home Services" :(
+"plumber",
+"gardener",
+"washer",
+"laundry",
+"dhobi",
+"batman",
+"servant",
+"maid",
+"driver",
+"chauffer",
+"guard",
+"security guard",
+"chokidaar",
+"chaukidar",
+"chowkidar",
+"maasi",
+"masi",
+"kaam wali",
+"cook",
+"aunty",
+"khala",
+"bua",
+"aaya",
+"baji",
+"nokrani"
+
+)
+"Furniture and Decor" :(
+"cupboard",
+"closet",
+"bed",
+"new dining table",
+"sofa",
+"vase",
+"pots and plants"
+)
+"Repair and Maintenance" :(
+"generator oil",
+"gate fixing",
+"paint",
+"gardening",
+
+)
+"Appliances" :(
+"washing machine",
+"refrigerator",
+"frisge",
+"deep freezer",
+"microwave",
+"oven",
+"iron",
+"juicer",
+"blender",
+"food processor"
+)
+
+}
